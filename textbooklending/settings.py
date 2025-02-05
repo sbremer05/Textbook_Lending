cf91da43df0ac9f277ef3bf316d1aa7d8ea23b65
almastelek <almasatelek@gmail.com>
2025-02-04 22:23:16 -0500

    Updated ALLOWED_HOSTS for Heroku

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-70dp93*)ixi37pnj^gu+*#&k8&qix1oz!e^$g6jl3wksuh%6&s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True" #True
 
-ALLOWED_HOSTS = ['textbooklending.herokuapp.com']
+ALLOWED_HOSTS = ['b-28-textbooklending-ec8819034608.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
