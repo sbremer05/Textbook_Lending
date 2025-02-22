b88a97df672ef36c0797bb6836da7038630a25e9
Ashwin Gopinathan <kaz9hp@virginia.edu>
2025-02-22 17:17:53 -0500

    Adding Patron dashboard, updating routes

--- login/urls.py
+++ login/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("logout/", views.logout_view, name="logout"),
-    path("redirect-after-login/", views.post_login_redirect, name="post_login"),
+    path("redirect-after-login/", views.post_login_redirect, name="post_login_redirect"),
     path("patron-dashboard/", views.patron_dashboard, name="patron_dashboard"),
     path("librarian-dashboard/", views.librarian_dashboard, name="librarian_dashboard"),
     path("choose-role/", views.choose_role, name="choose_role"),
