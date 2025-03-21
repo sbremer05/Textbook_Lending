a6579d92f4dce22f4c63c0f6df8cb0de1a2c411f
almastelek <almasatelek@gmail.com>
2025-03-21 17:47:14 -0400

    postgres attempt

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -128,17 +128,31 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     #     #'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+#     # 'default': dj_database_url.config(
+#     #     default= os.environ.get('DATABASE_URL'),
+#     #     conn_max_age=600
+#     # )
+#     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+# }
+if 'DATABASE_URL' in os.environ:
+    # Production (Heroku Postgres)
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-    # 'default': dj_database_url.config(
-    #     default= os.environ.get('DATABASE_URL'),
-    #     conn_max_age=600
-    # )
-}
+else:
+    # Local development (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
