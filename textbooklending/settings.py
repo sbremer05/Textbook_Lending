75a74f08c2ae85b237a085dfaef2a29e89d9fda5
almastelek <almasatelek@gmail.com>
2025-03-21 16:46:40 -0400

    back to sqlite

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -129,15 +129,15 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     #'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config(
-        default= os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config(
+    #     default= os.environ.get('DATABASE_URL'),
+    #     conn_max_age=600
+    # )
 }
 
 
