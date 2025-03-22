8faff33500970f54b6b25297e8239aaa3985914b
almastelek <almasatelek@gmail.com>
2025-03-22 16:59:22 -0400

    updated db logic in settings.py

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -143,7 +143,7 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 if 'DATABASE_URL' in os.environ:
     # Production (Heroku Postgres)
     DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.config(conn_max_age=600)
     }
 else:
     # Local development (SQLite)
