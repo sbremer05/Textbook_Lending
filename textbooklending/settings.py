ed78ae5d01aacdb730978594533fcc6cda70b2f3
almastelek <almasatelek@gmail.com>
2025-04-07 20:50:23 -0400

    temporary change for debugging 2

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -157,11 +157,8 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 #     }
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+    'default': dj_database_url.config(conn_max_age=600)
+}
 
 
 # Password validation
