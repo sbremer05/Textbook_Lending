5d781a005ad771d6c033a4a4b722ffe240264f62
almastelek <almasatelek@gmail.com>
2025-04-07 20:55:58 -0400

    changed site id for heroku

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = ['b-28-textbooklending-ec8819034608.herokuapp.com', 'localhost',
 
 # Application definition
 
-SITE_ID = 6
+SITE_ID = 4 # 6 if DEBUG else 4
 
 INSTALLED_APPS = [
     'django.contrib.admin',
