73bc37b458b14ea0a51ba1ad4ff52eb42558e16d
almastelek <almasatelek@gmail.com>
2025-04-15 15:56:19 -0400

    updated login/models.py for is_guest

--- login/models.py
+++ login/models.py
@@ -20,4 +20,4 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance, role="patron", is_setup=True)
+        Profile.objects.create(user=instance, role="patron", is_setup=True, is_guest=False)
