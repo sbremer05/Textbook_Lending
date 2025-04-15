99140ad6c0fe51b09a00b26a59f169f10f756ed9
almastelek <almasatelek@gmail.com>
2025-04-15 12:14:23 -0400

    temp debug

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -149,18 +149,22 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 # }
 #if 'DATABASE_URL' in os.environ:
 
-if not DEBUG:
-    # Production (Heroku Postgres)
-    DATABASES = {
+# if not DEBUG:
+#     # Production (Heroku Postgres)
+#     DATABASES = {
+#         'default': dj_database_url.config(conn_max_age=600)
+#     }
+# else:
+#     # Local development (SQLite)
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
         'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    # Local development (SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
 
 
