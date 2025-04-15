d56cd691957b422b2f164c2b23d404f2e044209e
almastelek <almasatelek@gmail.com>
2025-04-15 17:48:01 -0400

    updated tests

--- login/tests.py
+++ login/tests.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.core.files.uploadedfile import SimpleUploadedFile
 from login.models import Profile
 from allauth.socialaccount.models import SocialApp
+from django.contrib.sites.models import Site
+
 class LoginTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='testpass')
@@ -13,7 +15,6 @@ class LoginTests(TestCase):
             client_id='dummy-client-id',
             secret='dummy-secret'
         )
-        from django.contrib.sites.models import Site
         current_site = Site.objects.get_current()
         self.social_app.sites.add(current_site)
         self.client = Client()
@@ -24,10 +25,9 @@ class LoginTests(TestCase):
 
     def test_profile_created(self):
         self.assertIsNotNone(self.user.profile)
-        self.assertEqual(str(self.user.profile), f"{self.user.username} - No Role Selected")
+        self.assertEqual(str(self.user.profile), f"{self.user.username} - patron")  # updated to reflect default role
 
     def test_profile_picture_upload_view_accessible(self):
         self.client.login(username='testuser', password='testpass')
         response = self.client.get(reverse('upload_profile_picture'))
         self.assertEqual(response.status_code, 200)
-
