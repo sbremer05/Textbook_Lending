ef6858fe690a5978acdbcc2a84779bfd97fd3ecf
almastelek <almasatelek@gmail.com>
2025-04-15 15:28:46 -0400

    temp debug

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -164,9 +164,21 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 #         }
 #     }
 
-DATABASES = {
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
         'default': dj_database_url.config(conn_max_age=600)
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
