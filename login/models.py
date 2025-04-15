7dc02dd1f81c7da78fb6d01269775810b7d05159
almastelek <almasatelek@gmail.com>
2025-04-15 15:49:23 -0400

    updated login/models.py to auto create user as patron

--- login/models.py
+++ login/models.py
@@ -20,4 +20,4 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, role="patron", is_setup=True)
