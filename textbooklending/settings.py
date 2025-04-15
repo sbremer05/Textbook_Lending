aa23e8d193c8e5a6393328a0276456130d9c8dec
almastelek <almasatelek@gmail.com>
2025-04-15 11:34:21 -0400

    temporary debug

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-70dp93*)ixi37pnj^gu+*#&k8&qix1oz!e^$g6jl3wksuh%6&s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True" #True
-#DEBUG = True
+# DEBUG = os.getenv("DEBUG", "False") == "True" #True
+DEBUG = True
 
 ALLOWED_HOSTS = ['b-28-textbooklending-ec8819034608.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -147,18 +147,22 @@ WSGI_APPLICATION = 'textbooklending.wsgi.application'
 # }
 #if 'DATABASE_URL' in os.environ:
 
-if not DEBUG:
-    # Production (Heroku Postgres)
-    DATABASES = {
+# if not DEBUG:
+#     # Production (Heroku Postgres)
+#     DATABASES = {
+#         'default': dj_database_url.config(conn_max_age=600)
+#     }
+# else:
+#     # Local development (SQLite)
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+
+DATABASES = {
         'default': dj_database_url.config(conn_max_age=600)
-    }
-else:
-    # Local development (SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
     }
 
 
