00e72a6c6d19d12a1b06f14f5d677f4ec7a2f9ac
Ashwin Gopinathan <kaz9hp@virginia.edu>
2025-04-28 22:19:26 -0400

    Resetting settings

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(".env")
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
