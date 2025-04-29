51e3cb84801f62d28e7e49e75427cf849245ef6e
Ashwin Gopinathan <165319531+A-Gopinathan@users.noreply.github.com>
2025-04-28 23:24:33 -0400

    Revert "Lib dashboard clarity"

--- textbooklending/settings.py
+++ textbooklending/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(".env")
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
